6f3b390ad85deb744106049170aba2592c2e9024
pariwut chansawang <pariwut.cha@ntb.co.th>
2022-04-25 10:33:13 +0700

    Update Page Takza555

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Takza555 from './Takza555';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Tak
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <Takza555></Takza555>
       </header>
     </div>
   );
---
+++ src/Takza555.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Takza555() {
+  return (
+      
+        <p>
+         lnwtakzaza 5555
+        </p>
+  );
+}
+
+export default Takza555;
